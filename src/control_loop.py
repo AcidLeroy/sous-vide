b1afb5f63927ce2e52a71cc1374f38320bce27c2
Cody Eilar <cody.eilar@gmail.com>
2018-04-08 20:18:28 +0000

    Create *simple* control loop
    - Loop simply turns relay on if too cold, off otherwise.
    
    Signed-off-by: Cody Eilar <cody.eilar@gmail.com>

--- src/control_loop.py
+++ src/control_loop.py
@@ -1,20 +1,40 @@
-from mock_control import MockControl
-import numpy as np
-import matplotlib.pyplot as plt
+from sous_videdb import SousVideDB
+import time
 
-class MockSousVideController(object):
-    def __init__(self):
-        a = np.arange(0, 2*math.pi, .001)
-        self.vals = np.sin(a)
-        pass
+def manual_mode(db): 
+    # Nothing needs to be done in manual mode. 
+    # Somone may be setting the relays on and off else where. 
+    print('manual mode')
+    pass
 
-    @property
-    def temperature(self):
-        pass
-    @temperature.getter(self):
-    def temperature()
+def automatic_mode(db): 
+    # In automatic mode we are going to be reading and writing to and from the 
+    # sensors based on the set point
+    print('automatic mode') 
+    set_point = db.set_point[1]
+    current_temp = db.current_temperature[1]
+    print('set_point = ', set_point, ', current_temp = ', current_temp)
+    if current_temp < set_point: 
+        print('Turning relay on ')
+        db.relay_on = True
+    else: 
+        print('Turning relay of ')
+        db.relay_on = False
+
+states = {'manual': manual_mode, 
+        'auto': automatic_mode} 
+
+def control_loop(db): 
+    while True: 
+        mode = db.mode[1]
+        # Execute the mode
+        states[mode](db) 
+        time.sleep(1)
+
 
 def main():
+    db = SousVideDB()
+    control_loop(db)
 
 
 if __name__ == '__main__':
